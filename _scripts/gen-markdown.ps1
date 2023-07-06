bbdf09af3af977b4813e911674cbcd698fd40cb8
brianddk <brianddk@users.noreply.github.com>
2023-07-05 20:13:16 -0500

    Updates width to CSS instead

--- _scripts/gen-markdown.ps1
+++ _scripts/gen-markdown.ps1
@@ -1,4 +1,23 @@
-﻿$img_txt = @'
+﻿$header = @'
+---
+layout: post
+title:  "AirBnB Austin Listing by Susana"
+date:   2023-07-04 16:00:00 -0500
+categories: airbnb review
+---
+
+<style type="text/css">
+    img, video {
+        width: 480px;
+        max-width: 480px;
+    }
+</style>
+
+This is a review of the [listing #886912477107293975](https://www.airbnb.com/rooms/886912477107293975/) ([archive](https://archive.today/6tREd)) in Austin by Susana.
+
+'@
+
+$img_txt = @'
 
 Change this line and "alt:text" text, "title:text" text "height" and "width" below as desired
 
@@ -7,11 +26,9 @@ Change this line and "alt:text" text, "title:text" text "height" and "width" bel
 $img_tag = @'
 
 <p>
-    <img src="filepath" 
-         alt="alt:filename" 
-         title="filename" 
-         width="480" 
-         height="320" />
+    <a href="filepath">
+        <img src="filepath" alt="alt:filename" title="filename"/>
+    </a>
 </p>
 
 '@
@@ -25,23 +42,26 @@ Change this line and "height" and "width" as desired
 $vid_tag = @'
 
 <p>
-    <video width="480" height="320" controls="controls">
+    <video controls="controls">
         <source src="filepath" type="video/mp4">
     </video>
 </p>
 
 '@
 
-$files = Get-ChildItem ..\assets\img
+$markdown = $header
+$files = (Get-ChildItem "..\assets\img") + (Get-ChildItem "..\assets\mov") | Sort-Object -Property "Name"
 foreach ($file in $files) {
     $filename = $file.BaseName
     $filepath = $file.Name
     if ($file.Extension -eq ".jpg") {
         $tag = $img_tag.Replace("filepath", "/assets/img/$filepath").Replace("filename", "$filename")
-        Write-Host "$img_txt $tag"
+        $markdown += "$img_txt `n $tag `n"
     }
     if ($file.Extension -eq ".mp4") {
-        $tag = $vid_tag.Replace("filepath", "/assets/img/$filepath")
-        Write-Host "$vid_txt $tag"
+        $tag = $vid_tag.Replace("filepath", "/assets/mov/$filepath")
+        $markdown += "$vid_txt `n $tag `n"
     }
 }
+
+Out-File -FilePath "..\_posts\2023-06-29-airbnb-austin-by-susana.markdown" -InputObject $markdown -Encoding ASCII
